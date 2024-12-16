5d720629b2916363deea718b3498481ea82e2e06
dellorl <lucas.dello.russo@gmail.com>
2024-12-16 11:18:50 -0300

    añado estilos y transiciones

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Aplicación React - Dello Russo Lucas
 
 En grupo junto con: Federico De Brito
+
+Para iniciar la aplicación, usar:
+
+# npm install
+
+# npm start
--- src/components/TarjetaPaciente.jsx
+++ src/components/TarjetaPaciente.jsx
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import '../css/TarjetaPaciente.css'
 
 const TarjetaPaciente = () => {
+    const [mostrarDatos, setMostrarDatos] = useState(false);
+    const toggleDatos = () => {
+        setMostrarDatos(!mostrarDatos);
+    }
+
     let nombre = "Jorge"
     let edad = 35
     let genero = "Masculino"
     let dni = 123456789
-    let fotoPerfil =
-        <img src="https://www.fundacionkonex.org/custom/web/data/imagenes/repositorio/2017/08/01/13082/201708010905081ec284aacbb965c8e938932758592a26.jpg" alt="foto de perfil" />
+    let fotoPerfil = <img src="https://www.fundacionkonex.org/custom/web/data/imagenes/repositorio/2017/08/01/13082/201708010905081ec284aacbb965c8e938932758592a26.jpg" alt="foto de perfil" />
+
+
     return (
         <>
-            <p>
-                Hola soy el componente TarjetaPaciente, ¡mira tus datos!
-            </p>
+            <div className="card-wrapper">
+                <p className="text-title">
+                    Hola, soy el componente TarjetaPaciente. ¡Mira tus datos!
+                </p>
 
-            <button>Ver Datos</button>
+                <button className="button-show-data" onClick={toggleDatos}>
+                    {mostrarDatos ? "Ocultar Datos" : "Ver Datos"}
+                </button>
 
-            <p>
-                nombre: {nombre},
-                edad: {edad},
-                genero: {genero},
-                dni: {dni}
-            </p>
+                <div className={`data-container ${mostrarDatos ? "visible" : "oculto"}`}>
+                    <ul className="list-data">
+                        <li>Tu Nombre: {nombre}</li>
+                        <li>Tu Edad: {edad}</li>
+                        <li>Tu Genero: {genero}</li>
+                        <li>Tu Dni: {dni}</li>
+                    </ul>
 
-            <p>
-                Tu foto de perfil: {fotoPerfil}
-            </p>
+
+                    <p className="text-profile-picture">
+                        Tu foto de perfil:
+                    </p>
+
+                    <div className="foto-perfil">
+                        {fotoPerfil}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
